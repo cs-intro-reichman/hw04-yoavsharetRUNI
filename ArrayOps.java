0ca7b8b28b905fc711272fcdf03c540bf2adc558
yoavsharetRUNI <yoav.sharet@post.runi.ac.il>
2024-01-12 11:54:44 +0200

    finished findMissingInt; before AutoGrade

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,11 +1,22 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+        int[] test = {0, 1, 3, 4, 5, 2, 7 ,8, 9, 10, 11};
+        System.err.println(findMissingInt(test));
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int arrayLength = array.length;
+        int full_sum = 0;
+        // calc the sum of all integers in the range [0, array.length]
+        for(int i =0; i<=arrayLength; i++){
+            full_sum +=i;
+        }
+        // full_sum - sum(input_array) = the missing digit
+        for(int i=0; i<arrayLength; i++){
+            full_sum -= array[i];
+        }
+        // full sum now contains the missing digit;
+        return full_sum;
     }
 
     public static int secondMaxValue(int [] array) {
